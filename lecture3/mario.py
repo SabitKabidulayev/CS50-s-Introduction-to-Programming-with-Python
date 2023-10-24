5a448e02ab0a84788877fded9a77b4dfd25d2d77
SabitKabidulayev <kabidulayevs@gmail.com>
2023-10-24 16:28:54 +0600

    change folder names

--- lecture3/mario.py
+++ lecture3/mario.py
@@ -1,11 +1,12 @@
 def main():
-    print_square(3)
+    height = int(input("Height: "))
+    pyramid(height)
 
-def print_square(size):
-    for i in range(size):
-        print_row(size)
 
-def print_row(width):
-    print("#" * width)
+def pyramid(n):
+    for i in range(n):
+        print("#" * i)
 
-main()
+
+if __name__=="__main__":
+    main()
